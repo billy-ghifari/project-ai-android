e0069b2e4a40e375e2be4a0195e4ad52c068c880
billy-ghifari <ghifari.billy7@gmail.com>
2024-06-11 16:40:42 +0700

    Update | Login, Recommendation, Ad Rating

--- be-recommendations/database/seeders/DatabaseSeeder.php
+++ be-recommendations/database/seeders/DatabaseSeeder.php
@@ -12,9 +12,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // $this->call(PlatformSeeder::class);
-        // $this->call(UserSeeder::class);
         $this->call([
+            UserSeeder::class,
+            PlatformSeeder::class,
             RatingsPlatformSeeder::class,
         ]);
         // \App\Models\User::factory(10)->create();
